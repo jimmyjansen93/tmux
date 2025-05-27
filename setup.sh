69b67e3ce7b95d9349642b83cf57418fbdd96193
Jimmy Jansen <jimmyjansen@storyteq.com>
2025-05-27 22:36:00 +0200

    Update configuration files for tmux

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+log() {
+  green='\033[0;32m'
+  nc='\033[0m'
+  echo -e "${green}LOG:${nc} $1"
+}
+
+error() {
+  red='\033[0;31m'
+  nc='\033[0m'
+  echo -e "${red}ERROR: $1${nc}"
+}
+
+warning() {
+  orange='\033[0;38;5;208m'
+  nc='\033[0m'
+  echo -e "${orange}WARNING:${nc} $1"
+}
+
+if [[ "$(uname -s)" != "Darwin" ]]; then
+  error "This script only works on macOS."
+  return 1
+fi
+
+if ! command -v brew >/dev/null 2>&1; then
+  error "Brew needs to be installed for this script"
+  return 1
+fi
+
+log "Installing tmux"
+brew install tmux
